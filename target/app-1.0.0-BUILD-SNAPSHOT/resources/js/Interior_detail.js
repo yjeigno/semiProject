d354f5b1dbcdf3b86da4a4bc044ff8e2ae940d34
Horsebeee <jyjun83@naver.com>
2022-12-05 16:56:24 +0900

    타겟

--- target/app-1.0.0-BUILD-SNAPSHOT/resources/js/Interior_detail.js
+++ target/app-1.0.0-BUILD-SNAPSHOT/resources/js/Interior_detail.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     ho_tag()
     ho_arrow()
+    ho_case()
 
     function ho_tag(){
         $('.Interior_img0').hover(function(){
@@ -38,6 +39,7 @@ $(document).ready(function(){
 
 
     }
+
     function ho_arrow(){
 
         $('.Interior_btn').mouseleave(function(){
@@ -60,6 +62,108 @@ $(document).ready(function(){
             }
         )}
 
+    function ho_case(){
+        $('#Interior_box1').hover(function (){
+
+            $('#btn1').css({
+                opacity:'1'
+            })
+            $('#arrow1').css({
+                display:'block'
+            })
+        })
+        $('#Interior_box1').mouseleave(function (){
+
+            $('#btn1').css({
+                opacity:'0'
+            })
+            $('#arrow1').css({
+                display:'none'
+            })
+        })
+
+        $('#Interior_box2').hover(function (){
+
+            $('#btn2').css({
+                opacity:'1'
+            })
+            $('#arrow2').css({
+                display:'block'
+            })
+        })
+
+        $('#Interior_box2').mouseleave(function (){
+
+            $('#btn2').css({
+                opacity:'0'
+            })
+            $('#arrow2').css({
+                display:'none'
+            })
+        })
+
+        $('#Interior_box3').hover(function (){
+
+            $('#btn3').css({
+                opacity:'1'
+            })
+            $('#arrow3').css({
+                display:'block'
+            })
+        })
+
+        $('#Interior_box3').mouseleave(function (){
+
+            $('#btn3').css({
+                opacity:'0'
+            })
+            $('#arrow3').css({
+                display:'none'
+            })
+        })
+
+        $('#Interior_box4').hover(function (){
+
+            $('#btn4').css({
+                opacity:'1'
+            })
+            $('#arrow4').css({
+                display:'block'
+            })
+        })
+
+        $('#Interior_box4').mouseleave(function (){
+
+            $('#btn4').css({
+                opacity:'0'
+            })
+            $('#arrow4').css({
+                display:'none'
+            })
+        })
+
+        $('#Interior_box5').hover(function (){
+
+            $('#btn5').css({
+                opacity:'1'
+            })
+            $('#arrow5').css({
+                display:'block'
+            })
+        })
+
+        $('#Interior_box5').mouseleave(function (){
+
+            $('#btn5').css({
+                opacity:'0'
+            })
+            $('#arrow5').css({
+                display:'none'
+            })
+        })
+
+    }
+ /// detail
 
 
 })
