5de09095ecae1498dd526dec058b39bb38d25fb3
Horsebeee <jyjun83@naver.com>
2022-12-09 00:40:45 +0900

    Ajaxpagination 값 받아오기, 결제 값 넘겨주기

--- src/main/java/com/hanssem/app/controller/DealRestController.java
+++ src/main/java/com/hanssem/app/controller/DealRestController.java
@@ -1,7 +1,10 @@
 package com.hanssem.app.controller;
 
 import com.hanssem.app.dto.ProductSizeColorDto;
+import com.hanssem.app.dto.QnaDto;
 import com.hanssem.app.service.DealService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,15 @@ import java.util.Map;
 public class DealRestController {
     @Autowired
     DealService dealService;
+
+    private static final Logger log = LogManager.getLogger(DealRestController.class);
+
+    /*
+    *  1. 가져올 범위의 쿼리문 작성
+    *  2. Ajax 호출 jQuery 작성
+    *  3. 페이지네이션할 범위 정하기
+    *  4. Ajax -> 페이징
+    * */
 
     @PostMapping(value = "/deal/color")
     public List<ProductSizeColorDto> sizeColorList(@RequestBody Map map) {
@@ -37,7 +49,18 @@ public class DealRestController {
 
         return sizeColorList;
     }
-//    @PostMapping("/deal/qna/{pageSize}/{page}")
-//    public List<>
+    @PostMapping(value = "/deal/qna")
+    public List<QnaDto> qnaList(@RequestBody Map map){
+        //pageSize -> page_size
+        System.out.println("2번값 : " + map.get("page_size"));
+        System.out.println("3번값 : " + map.get("product_number"));
+        System.out.println("4번값 : " + map.get("offset"));
+
+        System.out.println("map :" + map);
+
+        List<QnaDto> qnaList = dealService.getQna(map);
+        System.out.println("결과값 : "+qnaList.size());
+        return qnaList;
+    }
 
 }
---
+++ src/test/java/com/hanssem/app/controller/DealRestControllerTest.java
@@ -0,0 +1,20 @@
+package com.hanssem.app.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+public class DealRestControllerTest {
+
+    @Test
+    public void qnaList(@RequestBody Map map) {
+
+    }
+}
